8e8d4c59cb1d3e171011f083c0802d48b4ebd2f4
Carriat Lucas <lucas.carriat@uha.fr>
2025-11-18 16:36:37 +0100

    Delete .idea directory

--- tp3/tp3exo1C.py
+++ tp3/tp3exo1C.py
@@ -1,8 +1,24 @@
-Infa10 = 0
-Supa10EtInf15 = 0
-SupOuEgala15 = 0
-for i in range(10):
-    nombre = float(input(f"Entrez un nombre {i+1} compris entre 0 et 20 :"))
-    while nombre < 0 or nombre > 20:
-        print(f"Réessayez)
-        nombre = float(input(f"Entrez la valeur "))
+def main():
+    valeurs = []
+
+
+    while len(valeurs) < 10:
+        try:
+            valeur = float(input(f"Entrez la valeur #{len(valeurs) + 1} (entre 0 et 20) : "))
+            if 0 <= valeur <= 20:
+                valeurs.append(valeur)
+            else:
+                print("Valeur non valide, veuillez entrer un nombre entre 0 et 20.")
+        except ValueError:
+            print("Entrée non valide, veuillez entrer un nombre réel.")
+
+    compteur_inferieur_10 = sum(1 for v in valeurs if v < 10)
+    compteur_10_a_15 = sum(1 for v in valeurs if 10 <= v < 15)
+    compteur_superieur_15 = sum(1 for v in valeurs if v >= 15)
+
+    print(f"Nombre de valeurs strictement inférieures à 10 : {compteur_inferieur_10}")
+    print(f"Nombre de valeurs supérieures ou égales à 10 et inférieures strictement à 15 : {compteur_10_a_15}")
+    print(f"Nombre de valeurs supérieures ou égales à 15 : {compteur_superieur_15}")
+
+if __name__ == "__main__":
+    main()
